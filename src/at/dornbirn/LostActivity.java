4e913c81bf29b8db70307203875e9e3bb890faef
Merve <merveaksunger@gmail.com>
2012-05-07 09:00:31 +0200

    map

--- src/at/dornbirn/LostActivity.java
+++ src/at/dornbirn/LostActivity.java
@@ -1,13 +1,28 @@
 package at.dornbirn;
 
-import android.app.Activity;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
 import android.os.Bundle;
 
-public class LostActivity extends Activity {
+public class LostActivity extends MapActivity {
+	MapView mapView;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        MapView mapView = (MapView) findViewById(R.id.mapview);    
+        mapView.setBuiltInZoomControls(true);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+    
+    
 }
