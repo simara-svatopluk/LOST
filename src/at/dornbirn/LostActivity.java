bab746c56a3dc8bf2c8a7dc5b60a4922aa3c128a
Petr Izrael <p.izrael@seznam.cz>
2012-05-13 16:08:57 +0200

    starts service on app start

--- src/at/dornbirn/LostActivity.java
+++ src/at/dornbirn/LostActivity.java
@@ -3,6 +3,7 @@ package at.dornbirn;
 import com.google.android.maps.MapActivity;
 import com.google.android.maps.MapView;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class LostActivity extends MapActivity {
@@ -16,6 +17,8 @@ public class LostActivity extends MapActivity {
         
         MapView mapView = (MapView) findViewById(R.id.mapview);
         mapView.setBuiltInZoomControls(true);
+        
+        startService(new Intent(this, PositionUpdater.class)); // start positioning service
     }
 
 	@Override
