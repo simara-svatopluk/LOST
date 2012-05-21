ad9b85d7dc3f20ea2363e6d86b1adabc9b55102d
Merve <merveaksunger@gmail.com>
2012-05-21 12:25:50 +0200

    m

--- src/at/dornbirn/LostActivity.java
+++ src/at/dornbirn/LostActivity.java
@@ -1,26 +1,15 @@
 package at.dornbirn;
 
 import java.io.File;
-import java.util.List;
-
 import org.mapsforge.android.maps.MapActivity;
-import org.mapsforge.android.maps.MapController;
 import org.mapsforge.android.maps.MapView;
-import org.mapsforge.android.maps.overlay.Overlay;
-import org.mapsforge.android.maps.overlay.OverlayItem;
-import org.mapsforge.core.GeoPoint;
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.*;
 
 public class LostActivity extends MapActivity {
 	MapView mapView;
-	List<Overlay> mapOverlays;
-	Drawable drawable;
-	MyItemizedOverlay itemizedOverlay;
-	MapController mc;
 	
     /** Called when the activity is first created. */
     @Override
@@ -30,23 +19,9 @@ public class LostActivity extends MapActivity {
         MapView mapView = new MapView(this);
         mapView.setClickable(true);
         mapView.setBuiltInZoomControls(true);
-        mapView.setMapFile(new File("/sdcard/maps/albania.map"));
+        mapView.setMapFile(new File("/sdcard/maps/austria.map"));
         
         setContentView(mapView);
-        
-        mapOverlays = mapView.getOverlays();
-        drawable = this.getResources().getDrawable(R.drawable.marker_red);
-        itemizedOverlay = new MyItemizedOverlay(drawable);
-        
-        GeoPoint geoPoint = new GeoPoint(41.39469, 19.86782);
-        OverlayItem overlayitem = new OverlayItem(geoPoint," "," ");
-        itemizedOverlay.addOverlay(overlayitem);
-        mapOverlays.add(itemizedOverlay);
-        
-        mc = mapView.getController();
-        mc.setCenter(geoPoint);
-        mc.setZoom(5);
-        
         
         /*
          * TODO find out what's causing problem in your emulator(s)
