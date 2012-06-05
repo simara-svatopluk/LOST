8b2d748806ccce723202ed2a54ceaef31cefa56b
Merve <merveaksunger@gmail.com>
2012-06-05 16:25:07 +0200

    maps list and new map downloading

---
+++ src/at/dornbirn/DownloadNewMapActivity.java
@@ -0,0 +1,78 @@
+package at.dornbirn;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DownloadNewMapActivity extends Activity{
+	private ListView mapNames;  
+	private ArrayList<String> incomingDataList; 
+	BufferedReader reader = null;
+	
+	@Override 
+	 protected void onCreate(Bundle savedInstanceState) {  
+		 super.onCreate(savedInstanceState);
+		 setContentView(R.layout.maps_list);
+		 
+		 mapNames = (ListView) findViewById(R.id.listViewMaps);
+		 mapNames.setClickable(true);
+		 
+		 mapNames.setOnItemClickListener(new OnItemClickListener() { 			 
+				@Override
+				public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {								
+					Object o = mapNames.getItemAtPosition(position);  
+					File externalStorage = Environment.getExternalStorageDirectory(); 
+					String path = externalStorage.getAbsolutePath();
+
+					URL url;
+					try {
+						url = new URL("http://download.mapsforge.org/maps/europe/" + o.toString());
+						HttpURLConnection con = (HttpURLConnection) url.openConnection();
+						
+						reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+						File file = new File(path + File.separator + "maps" + File.separator + o.toString() + File.separator);
+						
+						FileOutputStream fos;
+						fos = new FileOutputStream(file);
+						
+						InputStream inputStream = con.getInputStream();
+						byte[] buffer = new byte[1024];
+						int bufferLength = 0;
+						
+						while((bufferLength = inputStream.read(buffer)) > 0){
+							fos.write(buffer, 0, bufferLength);
+						}
+						fos.close();
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+			} 
+		});
+		 	 
+		 Bundle extra = getIntent().getExtras();
+		 incomingDataList = extra.getStringArrayList("countries");  
+		 mapNames.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, incomingDataList));	 
+	 }
+
+}
---
+++ src/at/dornbirn/MapListActivity.java
@@ -0,0 +1,112 @@
+package at.dornbirn;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class MapListActivity extends Activity{
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.download);	
+    	
+    	Button button = (Button) findViewById(R.id.button1);
+    	button.setOnClickListener(new View.OnClickListener() {
+    		BufferedReader reader = null;
+    		private ArrayList<String> countries = new ArrayList<String>(); 
+			ArrayList<String> tokens = new ArrayList<String>();
+			
+			@Override
+			public void onClick(View v) {
+				try{
+					URL url = new URL("http://download.mapsforge.org/maps/europe/");
+					HttpURLConnection con = (HttpURLConnection) url.openConnection();
+					
+					reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+					String str = "";
+					
+					while((str = reader.readLine()) != null){
+						if(str.contains(".map\"") == true){
+							String tkn = "\"";
+							StringTokenizer tokenizer = new StringTokenizer(str, tkn);
+							
+							while(tokenizer.hasMoreElements()){
+								String token = (String) tokenizer.nextElement();
+								tokens.add(token);	
+							}							
+						}
+					}
+					
+					for(String var : tokens){
+						if(var.contains(".map") == true && var.contains(">") == false){
+							countries.add(var);	
+						}
+					}
+					
+					Intent intent = new Intent(getApplicationContext(), DownloadNewMapActivity.class); 
+					Bundle bundle = new Bundle();  
+					bundle.putStringArrayList("countries", countries);  
+					intent.putExtras(bundle);  
+					startActivity(intent);  
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
+				finally{
+					if(reader != null){
+						try{
+							reader.close();
+						}catch(IOException e){
+							e.printStackTrace();
+						}
+					}
+				}
+			}
+		});
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+    	new MenuInflater (this).inflate(R.menu.menu1, menu);
+    	
+		return super.onCreateOptionsMenu(menu);   
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+    	switch (item.getItemId())
+    	{
+    		case R.id.itemDownload:
+    			return true;
+    		case R.id.itemStats:
+    			startActivity(new Intent(this, StatisticsActivity.class));
+    		case R.id.itemNewPlan:	    		
+    			startActivity(new Intent(this, PlanActivity.class));
+    			return true;
+    		case R.id.itemCurrentPlan:
+    			startActivity(new Intent(this, LostActivity.class));
+    			return true;
+    		case R.id.itemSettings:
+    			startActivity(new Intent(this, SettingsActivity.class));
+    			return true;
+    	}
+    
+    	return super.onOptionsItemSelected(item);
+    }
+}
